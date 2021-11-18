8d93d69a8cdb44b3011159a5cb22e15506e771c7
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2021-11-18 14:00:55 +0900

    Vue : Search result is implemented

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,19 +1,36 @@
+import SearchModel from "./models/SearchModel.js";
+
 new Vue({
     el: "#app",
     data: {
         query: "",
+        submitted: false,
+        searchResult: [],
     },
     methods: {
         onSubmit(e) {
-            debugger;
+            this.search();
         },
         onReset(e) {
-            this.query = "";
-            console.log("onReset");
-            // ToDo 검색결과를 숨기는 로직 추가
+            this.resetForm();
+            this.resetResult();
         },
         onKeyup(e) {
             if (!this.query.length) this.onReset(e);
+        },
+        search() {
+            SearchModel.list().then((data) => {
+                this.searchResult = data;
+                this.submitted = true;
+            });
+        },
+
+        resetForm() {
+            this.query = "";
+        },
+        resetResult() {
+            this.searchResult = [];
+            this.submitted = false;
         },
     },
 });
