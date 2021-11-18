149eed69f879dd4f6cacb8b7eb1cb7fd6f7bc6d3
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2021-11-18 15:56:26 +0900

    Vue : Tab is implemented

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,4 +1,6 @@
 import SearchModel from "./models/SearchModel.js";
+import KeywordModel from "./models/KeywordModel.js";
+import HistoryModel from "./models/HistoryModel.js";
 
 new Vue({
     el: "#app",
@@ -6,10 +8,20 @@ new Vue({
         query: "",
         submitted: false,
         searchResult: [],
+        tabs: ["추천 검색어", "최근 검색어"],
+        selectedTab: "",
+        keywords: [],
+        history: [],
+    },
+    created() {
+        this.selectedTab = this.tabs[0];
+        this.fetchKeyword();
+        this.fetchHistory();
     },
     methods: {
-        onSubmit(e) {
-            this.search();
+        onSubmit(query) {
+            this.query = query;
+            this.search(query);
         },
         onReset(e) {
             this.resetForm();
@@ -18,19 +30,37 @@ new Vue({
         onKeyup(e) {
             if (!this.query.length) this.onReset(e);
         },
-        search() {
+        search(query) {
             SearchModel.list().then((data) => {
                 this.searchResult = data;
                 this.submitted = true;
             });
+            HistoryModel.add(query);
+            this.fetchHistory();
         },
-
         resetForm() {
             this.query = "";
         },
         resetResult() {
             this.searchResult = [];
             this.submitted = false;
+        },
+        onClickTab(tab) {
+            this.selectedTab = tab;
+        },
+        fetchKeyword() {
+            KeywordModel.list().then((data) => {
+                this.keywords = data;
+            });
+        },
+        fetchHistory() {
+            HistoryModel.list().then((data) => {
+                this.history = data;
+            });
+        },
+        deleteItem(keyword) {
+            HistoryModel.remove(keyword);
+            this.fetchHistory();
         },
     },
 });
