befa4f4bf86ffc97ff64e166da3a26d767779a21
GyeongHwan Kim <gyeonghwan.dev@gmail.com>
2021-11-19 01:57:17 +0900

    Add FormComponent

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -1,66 +1,71 @@
-import SearchModel from './models/SearchModel.js'
-import KeywordModel from './models/KeywordModel.js'
-import HistoryModel from './models/HistoryModel.js'
+import SearchModel from "./models/SearchModel.js";
+import KeywordModel from "./models/KeywordModel.js";
+import HistoryModel from "./models/HistoryModel.js";
+
+import FormComponent from "./components/FormComponent.js";
 
 new Vue({
-  el: '#app',
-  data: {
-    query: '',
-    submitted: false,
-    tabs: ['추천 검색어', '최근 검색어'],
-    selectedTab: '',
-    keywords: [],
-    history: [],
-    searchResult: []
-  },
-  created() {
-    this.selectedTab = this.tabs[0]
-    this.fetchKeyword()
-    this.fetchHistory()
-  },
-  methods: {
-    onSubmit(e) {
-      this.search()
+    el: "#app",
+    data: {
+        query: "",
+        submitted: false,
+        searchResult: [],
+        tabs: ["추천 검색어", "최근 검색어"],
+        selectedTab: "",
+        keywords: [],
+        history: [],
     },
-    onKeyup(e) {
-      if (!this.query.length) this.resetForm()
+    components: {
+        "search-form": FormComponent,
     },
-    onReset(e) {
-      this.resetForm()
+    created() {
+        this.selectedTab = this.tabs[0];
+        this.fetchKeyword();
+        this.fetchHistory();
     },
-    onClickTab(tab) {
-      this.selectedTab = tab
+    methods: {
+        onSubmit(query) {
+            this.query = query;
+            this.search(query);
+        },
+        onReset(e) {
+            this.resetForm();
+            this.resetResult();
+        },
+        onKeyup(e) {
+            if (!this.query.length) this.onReset(e);
+        },
+        search(query) {
+            SearchModel.list().then((data) => {
+                this.searchResult = data;
+                this.submitted = true;
+            });
+            HistoryModel.add(query);
+            this.fetchHistory();
+        },
+        resetForm() {
+            this.query = "";
+        },
+        resetResult() {
+            this.searchResult = [];
+            this.submitted = false;
+        },
+        onClickTab(tab) {
+            this.selectedTab = tab;
+        },
+        fetchKeyword() {
+            KeywordModel.list().then((data) => {
+                this.keywords = data;
+            });
+        },
+        fetchHistory() {
+            HistoryModel.list().then((data) => {
+                this.history = data;
+            });
+        },
+        deleteItem(keyword) {
+            HistoryModel.remove(keyword);
+            this.fetchHistory();
+        },
     },
-    onClickKeyword(keyword) {
-      this.query = keyword;
-      this.search()
-    },
-    onClickRemoveHistory(keyword) {
-      HistoryModel.remove(keyword)
-      this.fetchHistory()
-    },
-    fetchKeyword() {
-      KeywordModel.list().then(data => {
-        this.keywords = data
-      })
-    },
-    fetchHistory() {
-      HistoryModel.list().then(data => {
-        this.history = data
-      })
-    },
-    search() {
-      SearchModel.list().then(data => {
-        this.submitted = true
-        this.searchResult = data
-      })
-      HistoryModel.add(this.query)
-      this.fetchHistory()
-    },
-    resetForm() {
-      this.query = ''
-      this.submitted = false
-      this.searchResult = []
-    }
-  }
-})
+});
